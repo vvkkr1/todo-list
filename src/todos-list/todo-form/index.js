12cfe225f5f3583a939d0999676b668c168d4446
vvkkr1 <vivek.kumarr1107@gmail.com>
2023-12-07 23:55:03 +0530

    some-changes-todo-list

--- src/todos-list/todo-form/index.js
+++ src/todos-list/todo-form/index.js
@@ -1,22 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Todos from '../todos';
 
 const TodoForm = () => {
+  const[value,setValue]=useState("");
+  const[data,setData]=useState([]);
+
+  function handleOnSubmit(e){
+    e.preventDefault();
+    // console.log(value);
+
+    // setData((oldValues)=>{
+    //   return 
+    // })
+    setData((oldValues)=>{
+      return [...oldValues, value]
+    });
+    // console.log(value);
+    setValue("")
+  }
+  console.log(data);
+
+  // const handleOnDelete = (id) => {
+
+  //   setData((oldValues)=>{
+  //     return oldValues.filter((item,index)=>{
+  //          return id !==index;
+  //     })
+  //   })
+
+  // }
+
+
   return (
     <div className='todo-form'>
       <div className='todo-form-wrap'>
+        <form>
           <div className='input-type-wrap'>
               <input 
                 type="text" 
+                name='item'
                 className='input-type' 
-                onChange={"something"}
+                value={value}
+                onChange={(e)=>setValue(e.target.value)}
                 placeholder='write item name here...'
               />
           </div>
           <div className='btn-wrap'>
-            <button onClick={"handleOnClick"}>Add Item</button>
+            <button className='btn-input' onClick={handleOnSubmit}>Add Item</button>
           </div>
-      </div>
-        This is todo form
+          <div>
+            {/* <button onClick={handleOnDelete}>Delete</button> */}
+          </div>
+        </form>
+     </div>
+    <Todos/>
     </div>
   )
 }
--- src/todos-list/todos/index.js
+++ src/todos-list/todos/index.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Todos = () => {
   return (
-    <div>
-        This is Todos
+    <div className='todos-container'>
+      <div className='todos-list'>
+        <li>value</li>
+      </div>
+      <div className='delete-item-icon'>
+        <i className='fa fa-times'
+        />
+      </div>
     </div>
   )
 }
